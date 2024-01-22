bfae7ac839790825575afa69d80f24f64b7e217e
Yayat Nurhidayat <yayatnhtedu@gmail.com>
2024-01-22 12:42:27 +0700

    Initial Commit

---
+++ memeMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memeMaker
+//
+//  Created by Yayat Nurhidayat on 22/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
